95227d932648176b04e2fe0706256330b943fd3d
kagero <lj.chistova@yandex.ru>
2018-02-17 20:15:29 +0300

    дабавлена минификация хтмл и джс, сделаны зеленые линии, добавлена карта без маркера

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,13 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 59.939, lng: 30.323},
+    zoom: 17
+  });
+}
+
